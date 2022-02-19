524ddf413bc4ea1ce4dc2e96d91622192c100bfd
VasiliyPaikou <gtr208111@gmail.com>
2022-02-19 08:46:57 +0300

    feat: added new solution

--- src/index.js
+++ src/index.js
@@ -1,71 +1,14 @@
 
 
 module.exports = function check(str, bracketsConfig) {
-
-	function isClosedBrecket(ch) {
-		return [')', ']', '}', '2', '4', '6'].indexOf(ch) > -1; //проверяет является ли скобка закрывающей
+	let arrayCheck = bracketsConfig.map(x => x.join(''))
+	let i = 0;
+	while (i < arrayCheck.length) {
+	let current = arrayCheck[i]
+	if (str.includes(current)) {
+	str = str.replace(current, '');
+	i = 0;
+	} else i++;
 	}
-	let count = 0
-	let count7 = 0
-	let count8 = 0
-	let stack = []
-	let breckets = {
-		')': '(',
-		'}': '{',
-		']': '[',
-		'|': '|',
-		'2': '1',
-		'4': '3',
-		'6': '5',
-		'7': '7',
-		'8': '8',
+	return str.length === 0;
 	}
-
-	for (let i = 0; i < str.length; i++) {
-		const current = str[i]
-		if (current == '|' && count % 2 == 0) {
-			stack.push(current)
-			count += 1
-			continue
-		} else if (current == '|' && count % 2 !== 0) {
-			if (current !== stack.pop())
-				return false
-			count += 1
-			continue
-		}
-		if (current == '7' && count7 % 2 == 0) {
-			stack.push(current)
-			count7 += 1
-			continue
-		} else if (current == '7' && count7 % 2 !== 0) {
-			if (current !== stack.pop())
-				return false
-			count7 += 1
-			continue
-		}
-		if (current == '8' && count8 % 2 == 0) {
-			stack.push(current)
-			count8 += 1
-			continue
-		} else if (current == '8' && count8 % 2 !== 0) {
-			if (current !== stack.pop())
-				return false
-			count8 += 1
-			continue
-		}
-		if (isClosedBrecket(current)) {
-
-			if (breckets[current] !== stack.pop())
-				return false
-
-		} else {
-			stack.push(current)
-		}
-
-	}
-	return stack.length === 0
-
-
-}
-
-
